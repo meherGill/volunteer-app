c0493b5fb6bc14a0200437d759e0e4c92fe2b7f9
Magnuscake <saneyar.k@gmail.com>
2022-04-24 20:53:05 +1000

    chore: add endpoint to fetch all businesses

--- src/pages/api/aid.ts
+++ src/pages/api/aid.ts
@@ -1,56 +1,53 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { PutItemCommand, ScanCommand } from "@aws-sdk/client-dynamodb";
-import { v4 as uuidv4 } from "uuid";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PutItemCommand, ScanCommand } from '@aws-sdk/client-dynamodb'
+import { v4 as uuidv4 } from 'uuid'
 
-import { dynamodb } from "@lib/dynamo-db";
+import { dynamodb } from '@lib/dynamo-db'
 
-const TABLE_NAME = "AidRequest";
+const TABLE_NAME = 'AidRequest'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  return new Promise((resolve) => {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  return new Promise(resolve => {
     switch (req.method) {
-      case "GET":
-        handleGetRequest(req, res);
-        break;
+      case 'GET':
+        handleGetRequest(req, res)
+        break
 
-      case "POST":
-        handlePostRequest(req, res);
-        break;
+      case 'POST':
+        handlePostRequest(req, res)
+        break
 
       default:
-        res.status(404).json({ success: false });
+        res.status(404).json({ success: false })
     }
-    return resolve;
-  });
+    return resolve
+  })
 }
 
 const handleGetRequest = async (_: NextApiRequest, res: NextApiResponse) => {
   const getParams = {
     TableName: TABLE_NAME,
-  };
+  }
 
-  const command = new ScanCommand(getParams);
+  const command = new ScanCommand(getParams)
 
   try {
-    const response = await dynamodb.send(command);
+    const response = await dynamodb.send(command)
 
     if (!response.Items) {
-      res.status(404).end("No requests for aid at the moment");
+      res.status(404).end('No requests for aid at the moment')
     }
-    res.status(response.$metadata.httpStatusCode! || 200).json(response.Items);
+    return res.status(response.$metadata.httpStatusCode! || 200).json(response.Items)
   } catch (err) {
-    res.status(500).end(err);
+    return res.status(500).end(err)
   }
-};
+}
 
 const handlePostRequest = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { email, address, description } = req.body;
+  const { email, address, description } = req.body
 
   if (!email || !address || !description) {
-    res.status(400).send("One or more fields are missing");
+    res.status(400).send('One or more fields are missing')
   }
 
   const putParams = {
@@ -70,16 +67,16 @@ const handlePostRequest = async (req: NextApiRequest, res: NextApiResponse) => {
       },
     },
     TableName: TABLE_NAME,
-  };
+  }
 
-  const cmd = new PutItemCommand(putParams);
+  const cmd = new PutItemCommand(putParams)
 
   try {
-    const response = await dynamodb.send(cmd);
+    const response = await dynamodb.send(cmd)
     return res
       .status(response.$metadata.httpStatusCode! || 200)
-      .end("Aid request created successfully");
+      .end('Aid request created successfully')
   } catch (err) {
-    return res.status(409).end(err);
+    return res.status(409).end(err)
   }
-};
+}
