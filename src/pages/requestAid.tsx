2d94bdf5ec94df53d918500bdc0e0c7a03c5e9e0
meherGill <meher310van@gmail.com>
2022-04-25 17:21:27 +1000

    dsvs
    efw

--- src/pages/requestAid.tsx
+++ src/pages/requestAid.tsx
@@ -1,170 +1,124 @@
-import GMaps from "@components/GMaps";
-import axios from "axios";
-import { useRouter } from "next/router";
-import React, { useEffect, useRef, useState } from "react";
-import Geocode from "react-geocode";
-import { ArrowLeftIcon } from "@heroicons/react/solid";
-const GCP_API_KEY = "AIzaSyBFvNDoVEJqDkF_pMJOBBMl97FLhpSJJ6A";
-import RequestAidModal from "@components/requestAidModal";
+import GMaps from '@components/GMaps'
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import React, { useEffect, useRef, useState } from 'react'
+import Geocode from 'react-geocode'
+import { ArrowLeftIcon } from '@heroicons/react/solid'
+const GCP_API_KEY = 'AIzaSyBFvNDoVEJqDkF_pMJOBBMl97FLhpSJJ6A'
+import RequestAidModal from '@components/requestAidModal'
 
 const RequestAid = () => {
-    Geocode.setApiKey(GCP_API_KEY);
-    const router = useRouter();
-    const [width, setWidth] = useState("");
-    const [height, setHeight] = useState("");
-    const [_marker, set_marker] = useState<Array<any>>([]);
-    const [showModal, set_showModal] = useState<boolean>(false);
-    var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0,
-    };
-    const [start_latLng, setStart_latLng] = useState({ lat: 0, lng: 0 });
-    function success(pos: any) {
-        var crd = pos.coords;
-        set_marker([
-            {
-                lat: crd.latitude,
-                lng: crd.longitude,
-                label: "current position",
-            },
-        ]);
-        setStart_latLng({ lat: crd.latitude, lng: crd.longitude });
-    }
+  Geocode.setApiKey(GCP_API_KEY)
+  const router = useRouter()
+  const [width, setWidth] = useState('')
+  const [height, setHeight] = useState('')
+  const [_marker, set_marker] = useState<Array<any>>([])
+  const [showModal, set_showModal] = useState<boolean>(false)
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  }
+  const [start_latLng, setStart_latLng] = useState({ lat: 0, lng: 0 })
+  function success(pos: any) {
+    var crd = pos.coords
+    set_marker([
+      {
+        lat: crd.latitude,
+        lng: crd.longitude,
+        label: 'current position',
+      },
+    ])
+    setStart_latLng({ lat: crd.latitude, lng: crd.longitude })
+  }
 
-    function error(err: any) {
-        console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
+  function error(err: any) {
+    console.warn(`ERROR(${err.code}): ${err.message}`)
+  }
 
-    // const handleSubmitForAid = (e: React.MouseEvent<any>) => {
-    //     e.preventDefault();
-    //     const description = (
-    //         document.querySelector(
-    //             "#requestAid_description"
-    //         ) as HTMLInputElement
-    //     ).value;
-    //     let latLng = {
-    //         lat: 0,
-    //         lng: 0,
-    //     };
-    //     const userData = JSON.parse(localStorage.getItem("userData") as string);
-    //     console.log("userData");
-    //     console.log(userData);
-    //     const input = document.querySelector(
-    //         "#addressForAid"
-    //     ) as HTMLInputElement;
-    //     const address = input.value;
+  const onClickSOS = () => {
+    set_showModal(true)
+  }
 
-    //     Geocode.fromAddress(address).then(
-    //         (res) => {
-    //             const { lat, lng } = res.results[0].geometry.location;
-    //             latLng.lat = lat;
-    //             latLng.lng = lng;
-    //             axios
-    //                 .post(URL_FOR_AID, {
-    //                     email: userData.email,
-    //                     address: address,
-    //                     description: description,
-    //                 })
-    //                 .then(
-    //                     (res) => {
-    //                         console.log(res);
-    //                         alert("Request posted successfully");
-    //                     },
-    //                     (err) => {
-    //                         console.log(err);
-    //                         alert("FAILED TO SUBMIT REQUEST");
-    //                     }
-    //                 );
-    //         },
-    //         (err) => {
-    //             console.log(err);
-    //             alert("Incorrect address");
-    //         }
-    //     );
-    // };
+  const onClickBackdrop = () => {
+    set_showModal(false)
+  }
 
-    const onClickSOS = () => {
-        set_showModal(true);
-    };
+  const clickBack = () => {
+    router.push('/VolunteerHome')
+  }
 
-    const onClickBackdrop = () => {
-        set_showModal(false);
-    };
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success, error, options)
+    // let _height = window.innerHeight;
+    let _width = window.innerWidth
+    let _height = window.innerHeight
+    setWidth(_width + 'px')
+    setHeight(_height + 'px')
+  }, [])
 
-    const clickBack = () => {
-        router.push("/VolunteerHome");
-    };
+  const onMapClickFunction = (e: any) => {
+    console.log(e.latLng.lat())
+    console.log(e.latLng.lng())
+    set_marker((marker: Array<any>) => {
+      let new_markers = [...marker]
+      if (new_markers.length > 1) {
+        new_markers.pop()
+      }
+      new_markers.push({
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        label: 'chosen location',
+      })
+      return new_markers
+    })
+  }
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(success, error, options);
-        // let _height = window.innerHeight;
-        let _width = window.innerWidth;
-        let _height = window.innerHeight;
-        setWidth(_width + "px");
-        setHeight(_height + "px");
-    }, []);
-
-    const onMapClickFunction = (e: any) => {
-        console.log(e.latLng.lat());
-        console.log(e.latLng.lng());
-        set_marker((marker: Array<any>) => {
-            let new_markers = [...marker];
-            if (new_markers.length > 1) {
-                new_markers.pop();
-            }
-            new_markers.push({
-                lat: e.latLng.lat(),
-                lng: e.latLng.lng(),
-                label: "chosen location",
-            });
-            return new_markers;
-        });
-    };
-
-    return (
-        <div className="w-screen h-screen">
-            <div className="z-0">
-                <GMaps
-                    width={width}
-                    height={height}
-                    start_lat={start_latLng.lat}
-                    start_long={start_latLng.lng}
-                    options={undefined}
-                    markers={_marker}
-                    callBackFunction={onMapClickFunction}
-                />
-            </div>
-            <div className="rounded-[50px] flex flex-row justify-start items-center z-10 absolute top-0 w-full bg-gray-800 p-3 border-4 border-gray-500">
-                <button
-                    className="w-10 h-3 flex items-center ml-3"
-                    onClick={clickBack}
-                >
-                    <ArrowLeftIcon color="white" />
-                </button>
-                <h1 className="w-full flex items-center justify-center text-2xl mr-5 font-raleway text-gray-200">
-                    REQUEST AID
-                </h1>
-            </div>
-            <div className="absolute w-full bottom-0 flex justify-center items-center">
-                <button
-                    className="btn btn-error z-10 rounded-full  w-24 h-24 mb-8 text-xl text-gray-50 drop-shadow-[0_35px_35px_rgb(0,0,0)]"
-                    onClick={onClickSOS}
-                >
-                    SOS
-                </button>
-            </div>
-            {showModal ? (
-                <div className="pls z-20 w-screen h-screen bg-gray-700/80 absolute top-0 flex justify-center">
-                    <div className="flex justify-center items-center h-full w-11/12">
-                        <RequestAidModal marker={_marker} />
-                    </div>
-                </div>
-            ) : (
-                ""
-            )}
+  return (
+    <div className="w-screen h-screen">
+      <div className="z-0">
+        <GMaps
+          width={width}
+          height={height}
+          start_lat={start_latLng.lat}
+          start_long={start_latLng.lng}
+          options={undefined}
+          markers={_marker}
+          callBackFunction={onMapClickFunction}
+        />
+      </div>
+      <div className="rounded-[50px] flex flex-row justify-start items-center z-10 absolute top-0 w-full bg-gray-800 p-3 border-4 border-gray-500">
+        <button className="w-10 h-3 flex items-center ml-3" onClick={clickBack}>
+          <ArrowLeftIcon color="white" />
+        </button>
+        <h1 className="w-full flex items-center justify-center text-2xl mr-5 font-raleway text-gray-200">
+          REQUEST AID
+        </h1>
+      </div>
+      <div className="absolute w-full bottom-0 flex justify-center items-center">
+        <button
+          className="btn btn-error z-10 rounded-full  w-24 h-24 mb-8 text-xl text-gray-50 drop-shadow-[0_35px_35px_rgb(0,0,0)]"
+          onClick={onClickSOS}
+        >
+          SOS
+        </button>
+      </div>
+      {showModal ? (
+        <div
+          className="pls z-20 w-screen h-screen bg-gray-700/80 absolute top-0 flex justify-center"
+          onClick={() => {
+            set_showModal(false)
+          }}
+        >
+          <div className="flex justify-center items-center h-full w-11/12">
+            <RequestAidModal marker={_marker} />
+          </div>
         </div>
-    );
-};
+      ) : (
+        ''
+      )}
+    </div>
+  )
+}
 
-export default RequestAid;
+export default RequestAid
